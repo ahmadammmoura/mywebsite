9a316fdd200952d1167e5b0471111112670004d0
ahmadammmoura <ammoura575@gmail.com>
2020-04-07 16:01:45 +0300

    almost done

--- home/home.js
+++ home/home.js
@@ -12,10 +12,25 @@ $(document).ready(function(){
 
 
     });
-    $('').on('click',function(){
-        $('.menu-toggler').removeClass('open');
-        $('.top-nav').removeClass('open');
 
+    
 
-    });
+});
+AOS.init({
+    once:true
+});
+
+function scrollTopAnimated() { 
+    $("html, body").animate({ scrollTop: "0" },2000); 
+    
+}
+
+$("a[href^='#']").click(function(e) {
+	e.preventDefault();
+	
+	var position = $($(this).attr("href")).offset().top;
+
+	$("body, html").animate({
+		scrollTop: position
+	}, 2000 );
 });
